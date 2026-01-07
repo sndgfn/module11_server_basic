50fd8f5a43e933c1521266ff0f546f15f5f32b4c
sndgfn <onikuzzaman113@gmail.com>
2026-01-08 03:30:16 +0600

    first commit

---
+++ src/server.ts
@@ -0,0 +1,21 @@
+import http,{IncomingMessage,Server,ServerResponse} from"http";
+
+const server:Server=http.createServer(
+    (req:IncomingMessage,res:ServerResponse)=>{
+        console.log("server is running");
+
+        if(req.url=='/'&& req.method=="get"){
+            res.writeHead(200,{"content-type" : "apalication/json"});
+            res.end(
+                JSON.stringify({
+                    messege:"hello from nodejs with typeScript.....",
+                    path:req.url
+                })
+            )
+        }
+    }
+)
+
+server.listen(5000,()=>{
+    console.log(`server is running on port ${5000}`);
+})
