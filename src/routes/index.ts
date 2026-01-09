536d28cebbb791f9bd95390044689ce484f394b0
sndgfn <onikuzzaman113@gmail.com>
2026-01-10 03:38:08 +0600

    parsbody

---
+++ src/routes/index.ts
@@ -0,0 +1,64 @@
+import { readUsers, writeUsers } from "../helpers/fileDb";
+import parseBody from "../helpers/parseBody";
+import addRoutes from "../helpers/RouteHandler";
+import sendJson from "../helpers/sendJson";
+
+addRoutes("GET", "/", (req, res) => {
+  sendJson(res, 200, {
+    message: "Hello from node js with typescript...",
+    path: req.url,
+  });
+});
+
+addRoutes("GET", "/api", (req, res) => {
+  sendJson(res, 200, {
+    message: "Health status ok",
+    path: req.url,
+  });
+});
+
+addRoutes("POST", "/api/users", async (req, res) => {
+  const body = await parseBody(req);
+
+  // user json read
+  const users = readUsers();
+
+  const newUser = {
+    ...body,
+  };
+
+  users?.push(newUser);
+
+  writeUsers(users);
+
+  sendJson(res, 201, { success: true, data: body });
+});
+
+addRoutes("PUT", "/api/users/:id", async (req, res) => {
+  const { id } = (req as any).params;
+  const body = await parseBody(req);
+
+  const users = readUsers();
+
+  const index = users.findIndex((user: any) => user.id == id);
+
+  if (index === -1) {
+    sendJson(res, 404, {
+      success: false,
+      message: "user not found",
+    });
+  }
+
+  users[index] = {
+    ...users[index],
+    ...body,
+  };
+
+  writeUsers(users);
+
+  sendJson(res, 202, {
+    success: true,
+    message: `id ${id} user updated`,
+    data: users[index],
+  });
+});
--- src/server.ts
+++ src/server.ts
@@ -1,59 +1,36 @@
 import http, { IncomingMessage, Server, ServerResponse } from "http";
-import config from "./config"
+import config from "./config";
+import { RouteHandler, routes } from "./helpers/RouteHandler";
+import "./routes";
+import findDynamicRoute from "./helpers/dynamicRouteHandler";
 
 const server: Server = http.createServer(
-    (req: IncomingMessage, res: ServerResponse) => {
-        console.log("server is running");
+  (req: IncomingMessage, res: ServerResponse) => {
+    const method = req.method?.toUpperCase() || "";
+    const path = req.url || "";
 
-        if (req.url == '/' && req.method == "get") {
-            res.writeHead(200, { "content-type": "apalication/json" });
-            res.end(
-                JSON.stringify({
-                    messege: "hello from nodejs with typeScript.....",
-                    path: req.url
-                })
-            )
-        }
-        //health route
-        if (req.url == '/api' && req.method == "GET") {
-            res.writeHead(200, { "appalication/json": "apalication/json" });
-            res.end(
-                JSON.stringify({
-                    messege: "health status ok",
-                    path: req.url
-                })
-            )
-        }
-        if (req.url == "/api/users" && req.method == "POST") {
-            // const user={
-            //     id:1,
-            //     name:"alice"
-            // }
-            //  res.writeHead(200, { "appalication/json": "apalication/json" });
-            // res.end(
-            //     JSON.stringify(user)
-            // )
-            let body = "";
+    const methodMap = routes.get(method);
+    const handler: RouteHandler | undefined = methodMap?.get(path);
 
-            //listen for data chunk
-            req.on("data", chunk => {
-                body += chunk.toSting();
-            })
-            req.on("end", () => {
-             try{
-                   const parsBody = JSON.parse(body);
-                console.log(parsBody);
-                console.log('catching current changes');
-                res.end(JSON.stringify(parsBody))
-                // res.end(body)
-             }catch(err:any){
-                console.log(err?.messege);
-             }
-            });
-        }
+    if (handler) {
+      handler(req, res);
+    } else if (findDynamicRoute(method, path)) {
+      const match = findDynamicRoute(method, path);
+      (req as any).params = match?.params;
+      match?.handler(req, res);
+    } else {
+      res.writeHead(404, { "content-type": "application/json" });
+      res.end(
+        JSON.stringify({
+          success: false,
+          message: "Route not found!!!",
+          path,
+        })
+      );
     }
-)
+  }
+);
 
-server.listen(5000, () => {
-    console.log(`server is running on port ${5000}`);
-})
+server.listen(config.port, () => {
+  console.log(`server is running on port ${config.port}`);
+});
