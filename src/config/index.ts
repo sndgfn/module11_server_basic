d46155816b645642ea29ae53c19505ead86d3187
sndgfn <onikuzzaman113@gmail.com>
2026-02-01 23:07:50 +0600

    index updated

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,3 +1,15 @@
+// import dotenv from "dotenv"
+// import path from "path"
+
+// dotenv.config({ path: path.join(process.cwd(), ".env") })
+
+// const config = {
+//     env: process.env.NOD_ENV ? Number(process.env.NOD_ENV) :5000,
+//     port:process.env.PORT
+// }
+
+// export default config
+
 import dotenv from "dotenv"
 import path from "path"
 
