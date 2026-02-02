38ca2250d5e7016f58d07d31d1e8a734125c65d7
sndgfn <onikuzzaman113@gmail.com>
2026-02-02 22:54:01 +0600

    input output funciton modified

--- src/data/inputOutput.java
+++ src/data/inputOutput.java
@@ -1,9 +1,36 @@
+// import java.util.Scanner;
+
+// public class Main {
+//     public static void main(String[] args) {
+
+//         Scanner sc = new Scanner(System.in);
+
+//         System.out.print("Enter first number: ");
+//         int a = sc.nextInt();
+
+//         System.out.print("Enter second number: ");
+//         int b = sc.nextInt();
+
+//         int sum = a + b;
+
+//         System.out.println("Sum = " + sum);
+
+//         sc.close();
+//     }
+// }
+
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
+
+    // BIG FUNCTION
+    public static String bigFunction() {
 
         Scanner sc = new Scanner(System.in);
+
+        // Take inputs
+        System.out.print("Enter your name: ");
+        String name = sc.nextLine();
 
         System.out.print("Enter first number: ");
         int a = sc.nextInt();
@@ -11,10 +38,34 @@ public class Main {
         System.out.print("Enter second number: ");
         int b = sc.nextInt();
 
+        // Calculations
         int sum = a + b;
+        int sub = a - b;
+        int mul = a * b;
+        double div = b != 0 ? (double) a / b : 0;
 
-        System.out.println("Sum = " + sum);
+        // Decision making
+        String bigger;
+        if (a > b) {
+            bigger = "First number is bigger";
+        } else if (a < b) {
+            bigger = "Second number is bigger";
+        } else {
+            bigger = "Both numbers are equal";
+        }
 
-        sc.close();
+        // Final output string (RETURN)
+        return "Hello " + name +
+                "\nSum: " + sum +
+                "\nSubtraction: " + sub +
+                "\nMultiplication: " + mul +
+                "\nDivision: " + div +
+                "\nResult: " + bigger;
+    }
+
+    public static void main(String[] args) {
+        // Call the big function
+        String result = bigFunction();
+        System.out.println(result);
     }
 }
