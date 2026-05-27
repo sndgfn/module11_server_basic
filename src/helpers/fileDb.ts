fb63099525e369cd711e55cffd7f57048090ab80
sndgfn <onikuzzaman113@gmail.com>
2026-05-27 19:03:26 +0600

    ki jni korwsi na buija

--- src/helpers/fileDb.ts
+++ src/helpers/fileDb.ts
@@ -1,12 +1,148 @@
-import fs from "fs"
-import path from 'path'
-const filePath = path.join(process.cwd(), "../src/data/users.json")
+// import fs from "fs"
+// import path from 'path'
+// const filePath = path.join(process.cwd(), "../src/data/users.json")
 
-export function readUsers() {
-    const data = fs.readFileSync(filePath, "utf-8")
-    return JSON.parse(data)
-}
+// export function readUsers() {
+//     const data = fs.readFileSync(filePath, "utf-8")
+//     return JSON.parse(data)
+// }
 
-export function writeUsers(users: any) {
-    fs.writeFileSync(filePath, JSON.stringify(users, null, 2))
-}
+// export function writeUsers(users: any) {
+//     fs.writeFileSync(filePath, JSON.stringify(users, null, 2))
+// }
+
+
+
+
+
+
+
+
+import { readUsers, writeUsers } from "../helpers/fileDb";
+import parseBody from "../helpers/parseBody";
+import addRoutes from "../helpers/RouteHandler";
+import sendJson from "../helpers/sendJson";
+
+addRoutes("GET", "/", (req, res) => {
+  sendJson(res, 200, {
+    message: "Hello from node js with typescript...",
+    path: req.url,
+  });
+});
+
+addRoutes("GET", "/api", (req, res) => {
+  sendJson(res, 200, {
+    message: "Health status ok",
+    path: req.url,
+  });
+});
+
+addRoutes("POST", "/api/users", async (req, res) => {
+  const body = await parseBody(req);
+
+  // user json read
+  const users = readUsers();
+
+  const newUser = {
+    ...body,
+  };
+
+  users?.push(newUser);
+
+  writeUsers(users);
+
+  sendJson(res, 201, { success: true, data: body });
+});
+
+addRoutes("PUT", "/api/users/:id", async (req, res) => {
+  const { id } = (req as any).params;
+  const body = await parseBody(req);
+
+  const users = readUsers();
+
+  const index = users.findIndex((user: any) => user.id == id);
+
+  if (index === -1) {
+    sendJson(res, 404, {
+      success: false,
+      message: "user not found",
+    });
+  }
+
+  users[index] = {
+    ...users[index],
+    ...body,
+  };
+
+  writeUsers(users);
+
+  sendJson(res, 202, {
+    success: true,
+    message: `id ${id} user updated`,
+    data: users[index],
+  });
+});
+import { readUsers, writeUsers } from "../helpers/fileDb";
+import parseBody from "../helpers/parseBody";
+import addRoutes from "../helpers/RouteHandler";
+import sendJson from "../helpers/sendJson";
+
+addRoutes("GET", "/", (req, res) => {
+  sendJson(res, 200, {
+    message: "Hello from node js with typescript...",
+    path: req.url,
+  });
+});
+
+addRoutes("GET", "/api", (req, res) => {
+  sendJson(res, 200, {
+    message: "Health status ok",
+    path: req.url,
+  });
+});
+
+addRoutes("POST", "/api/users", async (req, res) => {
+  const body = await parseBody(req);
+
+  // user json read
+  const users = readUsers();
+
+  const newUser = {
+    ...body,
+  };
+
+  users?.push(newUser);
+
+  writeUsers(users);
+
+  sendJson(res, 201, { success: true, data: body });
+});
+
+addRoutes("PUT", "/api/users/:id", async (req, res) => {
+  const { id } = (req as any).params;
+  const body = await parseBody(req);
+
+  const users = readUsers();
+
+  const index = users.findIndex((user: any) => user.id == id);
+
+  if (index === -1) {
+    sendJson(res, 404, {
+      success: false,
+      message: "user not found",
+    });
+  }
+
+  users[index] = {
+    ...users[index],
+    ...body,
+  };
+
+  writeUsers(users);
+
+  sendJson(res, 202, {
+    success: true,
+    message: `id ${id} user updated`,
+    data: users[index],
+  });
+});
